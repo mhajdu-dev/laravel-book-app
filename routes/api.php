d14f6d6cafa52ef7df85c67dc576610fd34b80df
Marci <hajdumarcel@gmail.com>
2023-07-17 14:09:45 +0200

    made a model and run migrations

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Recipe;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/products', function () {
+    return Recipe::all();
+});
+
+Route::post('/products', function () {
+});
 
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
